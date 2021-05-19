0fc61efe52754c59b2ea1c0420982ceb6cbf415f
Raj Parekh <raj.parekh@mcqdb.com>
2021-05-19 15:03:32 +0530

    init

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-let vUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=395&date=05-05-2021";
+let vUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=395&date=06-05-2021";
 
 let axios = require('axios');
+let fetch = require('node-fetch');
 
 const notifier = require('node-notifier');
 
+var config = {
+  headers: {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51'}
+};
+
+let vUrl2 = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=392&date=06-05-2021";
+
 async function main() {
-	console.log(`Status Update: ${new Date()} REQUESTING API`);
+	console.log(`Status Update:${vUrl + "&t=" + new Date().getTime()}`);
 
-	let manifest = await axios.get(vUrl);
+	let manifest = await axios.get(vUrl + "&t=" + new Date().getTime(), config);
+	let manifest2 = await axios.get(vUrl2 + "&t=" + new Date().getTime(), config);
 
-	let centres = manifest.data.centers;
-
-	let aged18 = centres.filter(_ => _.sessions.filter(_x => _x.min_age_limit == 18 && _x.available_capacity > 0).length > 0).map(x => `${x.name}, ${x.address}`)
+	 let centres = manifest.data.centers.concat(manifest2.data.centers);
+	let aged18 = centres.filter(_ => _.sessions.filter(_x => _x.min_age_limit == 18 && _x.available_capacity > 0).length > 0).map(x => `${x.pincode} ${x.name}, ${x.address}`)
 
 
 	if (aged18.length > 0)
 	{
-	console.log(`Status Update: ${new Date()} ${aged18}`);
+	console.log(`Status Update: ${new Date()} `);
+	console.log(`${JSON.stringify(aged18)}`)
 
 		notifier.notify({
 	  'title': 'VACCINE AVAILABLE',
@@ -27,16 +35,17 @@ async function main() {
 	  'sound': 'ding.mp3',
 	  'wait': true
 	});
-	notifier.on('click', (obj, options) => {
-	  const spawn = require('child_process').spawn;
-	  const cmd = spawn('open', ['https://www.cowin.gov.in/home']);
-	});
 	} else {
-	console.log(`Status Update: ${new Date()} No Vaccine for _x.min_age_limit == 18 && _x.available_capacity > 0`);
+		let for18 = centres.filter(_ => _.sessions.filter(_x => _x.min_age_limit == 18) > 0).length
+		let for45 = centres.filter(_ => _.sessions.filter(_x => _x.min_age_limit != 18) > 0).length
+		let for45Ava = centres.filter(_ => _.sessions.filter(_x =>  _x.available_capacity > 0) > 0).length
+
+
+		console.log(`Status Update: ${new Date()} ${for18} ${for45} ${for45Ava}`);
 
 	}
 
 }
 
 main();
-setInterval(main, 1000 * 60 * 2);
+setInterval(main, 1000 * 5);
--- justcheck.js
+++ justcheck.js
@@ -1,19 +1,29 @@
 let vUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=395&date=05-05-2021";
 
-let axios = require('axios');
 
 const notifier = require('node-notifier');
 
 async function main() {
-	console.log(`Status Update: ${new Date()} REQUESTING API`);
+	console.log(`Status Update: ${vUrl + "&t=" + new Date().getTime()} REQUESTING API`);
+	let axios = require('axios');
 
-	let manifest = await axios.get(vUrl + "&t" + new Date().getTime());
+
+	let manifest = await axios.get(vUrl + "&t=" + new Date().getTime());
 
 	let centres = manifest.data.centers;
 
-	console.log(centres);
+	if (!centres)
+	{
+		console.log(`invalid ${JSON.stringify(manifest.data)}`)
 
-	let aged18 = centres.filter(_ => _.sessions.filter(_x => _x.available_capacity > 0).length > 0).map(x => `${x.name}, ${x.address}`)
+		return
+	}
+
+	let filtered = centres.filter(_ => _.sessions.filter(_x => _x.available_capacity > 0).length > 0)
+
+	console.log(filtered.map(x => x.sessions));
+
+	let aged18 = filtered.map(x => `${x.pincode} ${x.name}, ${x.address} ${x.sessions.map(y => y.min_age_limit)} ${x.sessions.map(y => y.available_capacity)}`)
 
 
 	if (aged18.length > 0)
